7ddc1b46daaa740042b6b339b3bbc50011d312fd
What-XD <101012824+What-XD@users.noreply.github.com>
2025-01-05 19:07:45 +0500

    giveaway participating

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import json
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import grpc
 import giveaway_pb2_grpc
 import giveaway_pb2
+from fastapi.middleware.cors import CORSMiddleware
 
 class Item(BaseModel):
     initData: str
     token: str
 app = FastAPI()
+origins = [
+    "https://6cc2-95-105-125-92.ngrok-free.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 channel = grpc.aio.insecure_channel("localhost:50051")
 grpc_stub = giveaway_pb2_grpc.GreeterStub(channel)
 
@@ -15,6 +30,4 @@ grpc_stub = giveaway_pb2_grpc.GreeterStub(channel)
 async def index(item: Item):
     response = await grpc_stub.SayGiveaway(giveaway_pb2.GiveawayRequest(token=item.token, initData=item.initData))
 
-    return {
-        "response": response.json_message
-    }
+    return json.loads(response.json_message)
